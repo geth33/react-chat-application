a89c8af560d685ad0828237b3623b9fa4612a9be
geth33 <bryson.oar@hotmail.com>
2021-01-31 13:50:12 -0700

    Added Project Structure

---
+++ src/components/MyMessage.jsx
@@ -0,0 +1,17 @@
+const MyMessage = ({ message }) => {
+
+    if(message?.attachments?.length > 0){
+        return (
+            <img src={message.attachments[0].file} alt="message-attachment" className="message-image" style={{float: right}}/>
+            
+        )
+    }
+
+    return(
+        <div>
+            MyMessage
+        </div>
+    );
+}
+
+export default MyMessage;
